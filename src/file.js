efa9a4505fbae811f738abee41b2bd933c1180ec
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-03 13:39:26 -0500

    make the options extensible

--- src/file.js
+++ src/file.js
@@ -1,14 +1,28 @@
 const loaderUtils = require('loader-utils');
 const path = require('path');
 
+const defaultOptions = {
+  docsDirectory: 'docs'
+};
+
 const getPaths = function (context) {
 
-  const options = loaderUtils.getOptions(context);
+  const options = Object.assign(
+    {},
+    defaultOptions,
+    loaderUtils.getOptions(context)
+  );
+
+  if (!options.basePath) {
+
+    throw new Error('An absolute base path is required in the loader options.');
+
+  }
 
   const filename = loaderUtils.getCurrentRequest(context).split('!').pop();
   const currentPath = path.dirname(filename);
 
-  const basePath = path.join(options.basePath, 'docs');
+  const basePath = path.join(options.basePath, options.docsDirectory);
 
   return {
     basePath,
