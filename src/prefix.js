3b7fc7f3571fc3b30375aa8789f9d39f4d6a9fce
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-03 13:56:48 -0500

    generator to convert relative paths to prefixes

---
+++ src/prefix.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const getPrefix = (base, filename) => {
+
+  const parts = path.relative(base, filename).split(path.sep);
+  const doubleDots = parts.filter(part => part === '..');
+
+  parts.splice(0, doubleDots.length * 2);
+
+  return parts
+    .map(part => part.replace('.md', ''))
+    .filter(part => part !== 'readme')
+    .join('-')
+    ;
+
+};
+
+module.exports = {
+  getPrefix
+};
