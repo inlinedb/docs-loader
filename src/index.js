e23a32b3ad0394018c1f2090882c0be3440e53ff
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-03 13:57:21 -0500

    the docs loader

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const cheerio = require('cheerio');
+const {getLinkedModules} = require('./modules');
+const {convertLinksToHash} = require('./hash-links');
+const {getPaths} = require('./file');
+const {highlight} = require('./highlighter');
+const {idTitles} = require('./id-titles');
+const markdown = require('markdown-it');
+
+const md = markdown({
+  highlight,
+  html: true,
+  linkify: true,
+  typographer: true
+});
+
+module.exports = function (content) {
+
+  this.cacheable();
+
+  const {
+    basePath,
+    currentPath,
+    filename
+  } = getPaths(this);
+
+  const html = md.render(content);
+  const $html = cheerio.load(html);
+
+  const links = convertLinksToHash($html, basePath, currentPath, filename);
+
+  idTitles($html, basePath, filename);
+
+  const template = $html.html();
+
+  return `module.exports = 
+    (${JSON.stringify({template})}).template + 
+    ${getLinkedModules(links, currentPath)};
+  `;
+
+};
