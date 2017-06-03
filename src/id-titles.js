2b1cf12c301761ad184d4bf2046ea534fa2f092a
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-03 13:46:37 -0500

    set ids to titles for the links to work
    NOTE!!!!
    
    only readme.md should start with `#` titles
    the highest title in all the other files in the folder should be `##`

---
+++ src/id-titles.js
@@ -0,0 +1,64 @@
+const {getPrefix} = require('./prefix');
+const path = require('path');
+
+const isReadme = filename => path.basename(filename, '.md') === 'readme';
+const kebab = text => text.toLowerCase().replace(/\s+/g, '-');
+const getId = (prefix, text) => `${prefix}-${kebab(text)}`;
+
+const idH1s = ($, prefix) =>
+  $('h1')
+    .each((i, elem) => {
+
+      const $element = $(elem);
+
+      $element.attr('id', prefix);
+
+    });
+
+const idH2s = ($, prefix, isPrimary) =>
+  $('h2')
+    .each((i, elem) => {
+
+      const $element = $(elem);
+      let id = prefix;
+
+      /**
+       * presumption:
+       *  only readme.md will start with h1
+       *  any other file will start with h2
+       */
+
+      if (isPrimary) {
+
+        id = getId(prefix, $element.text());
+
+      }
+
+      $element.attr('id', id);
+
+    });
+
+const idH3s = ($, prefix) =>
+  $('h3')
+    .each((i, elem) => {
+
+      const $element = $(elem);
+
+      $element.attr('id', getId(prefix, $element.text()));
+
+    });
+
+const idTitles = ($, basePath, filename) => {
+
+  const prefix = getPrefix(basePath, filename);
+  const isPrimary = isReadme(filename);
+
+  idH1s($, prefix);
+  idH2s($, prefix, isPrimary);
+  idH3s($, prefix);
+
+};
+
+module.exports = {
+  idTitles
+};
