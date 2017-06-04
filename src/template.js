24a492607a5b668788f55fddaf88b389528b9ef3
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-04 00:09:10 -0500

    set cursor style

--- src/template.js
+++ src/template.js
@@ -7,7 +7,7 @@ const decorate = html => cheerio.load(`
   >
     ${html}
     
-    <a class="link-to-top" onclick="document.body.scrollTop = 0; window.location.hash = '';">
+    <a class="link-to-top" style="cursor: pointer" onclick="document.body.scrollTop = 0; window.location.hash = '';">
       <span style="font-size: 1.5em">&uarr;</span> Top
     </a>
   </section>  
